92096ead92dd671adc62b2176d58abcb445ac13d
Taiwo Joseph <taiwokelvin@gmail.com>
2021-05-02 14:57:06 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -5,10 +5,11 @@ import NewsCards from './components/NewsCards/NewsCards';
 import useStyles from './styles';
 import alanLogo from './images/alanLogo.jpg';
 import wordsToNumbers from 'words-to-numbers';
+import { Typography } from '@material-ui/core';
 
 function App() {
   const [newsArticles, setNewsArticles] = useState([]);
-  const [activeArticle, setActiveArticle] = useState(-1);
+  const [activeArticle, setActiveArticle] = useState(0);
   const classes = useStyles();
 
   const alanKey = 'd283499da95feef62597f07d25a4f7f42e956eca572e1d8b807a3e2338fdd0dc/stage';
@@ -24,21 +25,29 @@ function App() {
         } else if(command = 'open'){
           const parsedNumber = number.length > 2 ? wordsToNumbers(number, {fuzzy: true}) : number;
           const article = articles[parsedNumber - 1];
-          if(parsedNumber > 20){
-            alanBtn().playText('Pleas try that again');
-          } else {
+          if(parsedNumber > articles.length){
+            alanBtn().playText('Pleas try that again...');
+          } else if(article) {
             window.open(article.url, '_blank');
-            alanBtn().playText('Opening...')
+            alanBtn().playText('Opening...');
+          } else {
+            alanBtn().playText('Please try that again...')
           }
         }
-      }
-    })
+      },
+    });
   }, []);
 
   return (
     <>
     <div className={classes.logoContainer}>
-      <img src={alanLogo} alt="alan Logo" className={classes.alanLogo}/>
+      {newsArticles.length ? (
+          <div className={classes.infoContainer}>
+            <div className={classes.card}><Typography variant="h5" component="h2">Try saying: <br /><br />Open article number [4]</Typography></div>
+            <div className={classes.card}><Typography variant="h5" component="h2">Try saying: <br /><br />Go back</Typography></div>
+          </div>
+        ) : null}
+      <img src={alanLogo} alt="alan-Logo" className={classes.alanLogo}/>
     </div>
     <NewsCards articles={newsArticles} activeArticle={activeArticle}/>
     </>
--- src/components/NewsCard/NewsCard.js
+++ src/components/NewsCard/NewsCard.js
@@ -1,45 +1,44 @@
 import { Button, Card, CardActionArea, CardActions, CardContent, CardMedia, Typography } from '@material-ui/core'
 import React, {useState, useEffect, createRef} from 'react';
-import defaultImg from '../../images/defaultImg.jpg';
+import defaultImg from '../../images/defaultImg.png';
 import  useStyles from './styles';
-import classNames from 'classnames'
 
-const NewsCard = ({article: {
-    description,
-    publishedAt, source, title, url, urlToImage
-}, i, activeArticle}) => {
-    const classes = useStyles();
-    const [elRefs, setElRefs] = useState([]);
-    const scrollToRef = (ref) => window.scroll(0, ref.current.offsetTop - 50)
+const NewsCard = ({ article: { description, publishedAt, source, title, url, urlToImage }, activeArticle, i }) => {
+  const classes = useStyles();
+  const [elRefs, setElRefs] = useState([]);
+  const scrollToRef = (ref) => window.scroll(0, ref.current.offsetTop - 50);
 
-    useEffect(() => {
-        setElRefs((refs)=> Array(20).fill().map((_, j)=> refs[j] || createRef()))
-    }, []);
+  useEffect(() => {
+    window.scroll(0, 0);
 
-    useEffect(() => {
-        if(i === activeArticle && elRefs(activeArticle)){
-            scrollToRef(elRefs(activeArticle));
-        }
-    }, [i, activeArticle, elRefs])
-    return (
-        <Card ref={elRefs[i]} className={classNames(classes.card, activeArticle === i ? classes.activeCard : null)}>
-            <CardActionArea href={url} target='_blank'>
-                <CardMedia className={classes.media} image={urlToImage || defaultImg }/>
-                <div  className={classes.details}>
-                    <Typography variant='body2' color='textSecondary' component='h2'>{(new Date(publishedAt)).toDateString()}</Typography>
-                    <Typography variant='body2' color='textSecondary' component='h2'>{source.name}</Typography>
-                </div>
-                <Typography className={classes.title} gutterBottom variant='h5'>{title}</Typography>
-                <CardContent>
-                    <Typography variant='body2' color='textSecondary' component='p'>{description}</Typography>
-                </CardContent>
-            </CardActionArea>
-            <CardActions  className={classes.cardActions}>
-                <Button size='small' color='primary'>Learn More</Button>
-                <Typography variant='h5' color='textSecondary'>{i + 1}</Typography>
-            </CardActions>
-        </Card>
-    )
-}
+    setElRefs((refs) => Array(20).fill().map((_, j) => refs[j] || createRef()));
+  }, []);
 
-export default NewsCard
+  useEffect(() => {
+    if (i === activeArticle && elRefs[activeArticle]) {
+      scrollToRef(elRefs[activeArticle]);
+    }
+  }, [i, activeArticle, elRefs]);
+
+  return (
+    <Card ref={elRefs[i]} className={ activeArticle === i ? classes.activeCard : classes.card}>
+      <CardActionArea href={url} target="_blank">
+        <CardMedia className={classes.media} image={urlToImage || defaultImg} title={title} />
+        <div className={classes.details}>
+          <Typography variant="body2" color="textSecondary" component="h2">{(new Date(publishedAt)).toDateString()}</Typography>
+          <Typography variant="body2" color="textSecondary" component="h2">{source.name}</Typography>
+        </div>
+        <Typography className={classes.title} gutterBottom variant="h5" component="h2">{title}</Typography>
+        <CardContent>
+          <Typography variant="body2" color="textSecondary" component="p">{description}</Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions className={classes.cardActions}>
+        <Button size="small" color="primary" href={url}>Learn More</Button>
+        <Typography variant="h5" color="textSecondary" component="h2">{i + 1}</Typography>
+      </CardActions>
+    </Card>
+  );
+};
+
+export default NewsCard;
